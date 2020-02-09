c0054125f64f29c56828a8837dce5b716f9ed757
Nilay Kumar <nilaykumar07@gmail.com>
2020-02-08 19:33:54 -0600

    Started working on the main algorithm

--- wfc.cpp
+++ wfc.cpp
@@ -1,8 +1,10 @@
 
+#include<algorithm>
 #include<cstdio>
+#include<cmath>
+#include<random>
 #include<string>
 #include<vector>
-#include<algorithm>
 
 #include "bitmap_image.hpp"
 
@@ -92,6 +94,7 @@ class Cell {
     public:
         Pattern colorMatrix;
         int frequency;
+        static int totalFrequency;
         std::vector<Pattern> up, down, left, right;
     
     // empty constructor
@@ -108,6 +111,9 @@ class Cell {
     }
 
 };
+
+// Why do I need this...?
+int Cell::totalFrequency;
 
 std::ostream& operator<<(std::ostream &stream, const Cell &cell) {
     stream << "Cell{ \n\t" << cell.colorMatrix << "\n\tFrequency{ " << cell.frequency << " }\n";
@@ -155,11 +161,9 @@ std::vector<Cell> createCellSheet(const int width, const int height, const int N
                 for(int l = 0; l < N; ++l) {
                     rgb_t color;
                     inputImage.get_pixel(mod(i + k, width), mod(j + l, height), color);
-                    //std::cout << "LOG : Reading pixel (" << i + k << ", " << j + l << ") as color (" << +color.red << ", " << +color.green << ", " << +color.blue << ")." << std::endl;
                     // take into account the offset from the 1px borders
                     int ii = i + 1 + i * N + k;
                     int jj = j + 1 + j * N + l;
-                    //std::cout << "LOG : Setting cell sheet pixel (" << ii << ", " << jj << ")." << std::endl;
                     cellSheet.set_pixel(ii, jj, color);
                     colorMatrix[k * N + l] = color;
                 }
@@ -194,8 +198,6 @@ std::vector<Cell> createCellSheet(const int width, const int height, const int N
                 c.left.push_back(rawMatrix[i * width + mod(j - 1, width)]);
                 c.right.push_back(rawMatrix[i * width + mod(j + 1, width)]);
                 cells.push_back(c);
-                std::cout << "LOG: Found new cell at (" << i << ", " << j << ")." << std::endl;
-                //std::cout << c << std::endl;
             } else {
                 ++ duplicate->frequency;
                 // check the four directions around this rawMatrix element and add those patterns as allowable to the cell if they aren't already there
@@ -222,6 +224,61 @@ std::vector<Cell> createCellSheet(const int width, const int height, const int N
     return cells;
 }
 
+bool** initializeState(std::vector<Cell>& cellTypes, const int outputWidth, const int outputHeight) {
+    std::cout << "LOG : Constructing initial state." << std::endl;
+
+    // initialize a 2d array (with dimensions the dimension of the output) of arrays of booleans (of size the size of cellTypes) that holds the coefficients of the linear combinations of cellTypes
+    bool** state = new bool*[outputWidth * outputHeight];
+    for(int i = 0; i < outputWidth * outputHeight; ++i) {
+        state[i] = new bool[cellTypes.size()];
+        for(int j = 0; j < cellTypes.size(); ++j)
+            state[i][j] = true;
+    }
+    return state;
+}
+
+// compute the Shannon entropy of a state vector
+double shannonEntropy(bool* stateVector, std::vector<Cell>& cellTypes) {
+    double entropy = 0;
+    for(int i = 0; i < cellTypes.size(); ++i) {
+        // if the statevector is not allowed to collapse to the ith state, don't count it in the entropy
+        if(stateVector[i] == false)
+            continue;
+        double p = static_cast<double>(cellTypes.at(i).frequency) / Cell::totalFrequency;
+        entropy -= p * log(p);
+    }
+    return entropy;
+}
+
+// find the position of the first minimum-entropy state vector in the state
+int findMinimumEntropyState(bool** state, std::vector<Cell>& cellTypes, const int outputWidth, const int outputHeight) {
+    int min = -1;
+    int minPosition = -1;
+    for(int i = 0; i < outputWidth * outputHeight; ++i) {
+        double entropy = shannonEntropy(state[i], cellTypes);
+        // use of < here will stick with the first min we find
+        if(min == -1 or entropy < min) {
+            min = entropy;
+            minPosition = i;
+        }
+    }
+    return minPosition;
+}
+
+// check if a state vector has already been collapsed, i.e. only one non-false value
+bool isCollapsed(bool* stateVector, std::vector<Cell>& cellTypes) {
+    int numOfTrues = 0;
+    for(int i = 0; i < cellTypes.size(); ++i)
+        if(stateVector[i] == true)
+            ++ numOfTrues;
+    return (numOfTrues == 1);
+}
+
+// perform general clean up operations
+void cleanup(bool** state) {
+    delete[] state;
+}
+
 int main() {
 
     // cell size
@@ -233,7 +290,11 @@ int main() {
     const std::string cellSheetFilename = "cellSheet.bmp";
     // cell sheet border color
     const rgb_t borderColor = make_colour(0, 0, 255);
-    // list of CellTypes (pixel data and )
+
+    // width of output image
+    const int outputWidth = 16;
+    // height of output image
+    const int outputHeight = 16;
 
     std::cout << "LOG : Using " << filename << " as input, with N = " << N << " and writing cell sheet to " << cellSheetFilename << "." << std::endl;
 
@@ -249,13 +310,60 @@ int main() {
 
     const int height = inputImage.height();
     const int width = inputImage.width();
+    Cell::totalFrequency = width * height;
 
-    // Number of (overlapping) NxN cells
-    //const int numCells = height * width;
-
+    // analyze the input bitmap to extract the cell types
     std::vector<Cell> cellTypes = createCellSheet(width, height, N, borderColor, inputImage, cellSheetFilename);
-    for(Cell c : cellTypes)
-        std::cout << c << std::endl;
+    bool** state = initializeState(cellTypes, outputWidth, outputHeight);
+    // start at a random point (this is deterministic at the moment)
+    std::default_random_engine generator;
+    std::uniform_int_distribution<int> wDist(0, outputWidth - 1);
+    std::uniform_int_distribution<int> hDist(0, outputHeight - 1);
+    int minStateX = wDist(generator);
+    int minStateY = hDist(generator);
+    std::cout << "LOG : Output dimensions are (" << outputWidth << ", " << outputHeight << ")." << std::endl;
+    std::cout << "LOG : Starting at (" << minStateX << ", " << minStateY << ")." << std::endl;
+    int minState = minStateY * outputWidth + minStateX;
+
+
+    // TODO for now just do a few iterations
+    for(int t = 0; t < 1; ++t) {
+        // find minimum entropy state
+        // if this is the first iteration, start at the vector chosen randomly above
+        if(t !=0) {
+            minState = findMinimumEntropyState(state, cellTypes, outputWidth, outputHeight);
+            minStateX = minState % outputWidth;
+            minStateY = minState / outputWidth;
+        }
+        // construct the relevant probability distribution
+        std::vector<int> frequencies;
+        for(int i = 0; i < cellTypes.size(); ++i)
+            // for each allowable cellType, take into account how often that cellType appears
+            if(state[minState][i])
+                frequencies.push_back(cellTypes.at(i).frequency);
+            else
+                frequencies.push_back(0);
+        std::discrete_distribution<int> dist(frequencies.begin(), frequencies.end());
+        int collapsedTo = dist(generator);
+        std::cout << "LOG : Collapsing state vector (" << minStateX << ", " << minStateY << ") to cell type " << collapsedTo << std::endl;
+        std::cout << "LOG : Before collapse:" << std::endl;
+        for(int i = 0; i < cellTypes.size(); ++i)
+            std::cout << state[minState][i] << " ";
+        std::cout << std::endl;
+
+        // collapse the state vector
+        for(int i = 0; i < cellTypes.size(); ++i)
+            if(i != collapsedTo)
+                state[minState][i] = false;
+        std::cout << "LOG : Verifying collapse:" << std::endl;
+        for(int i = 0; i < cellTypes.size(); ++i)
+            std::cout << state[minState][i] << " ";
+        std::cout << std::endl;
+
+        // propogate the changes
+        // TODO if a state vector has no support then we've run into a contradiction
+    }
+    cleanup(state);
 
     return 0;
 }
